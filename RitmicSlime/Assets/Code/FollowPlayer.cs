d361c63bd9af07f9db71cc1c9b1482f9ce970614
Manuel Lecaro <mnlecaro@gmail.com>
2019-06-11 22:17:38 -0500

    Uploading final changes

--- RitmicSlime/Assets/Code/FollowPlayer.cs
+++ RitmicSlime/Assets/Code/FollowPlayer.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour {
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform trans;
+    public movePlayer player;
+    public float offsetY;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        trans = this.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //trans.position = new Vector3(10, 10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPos = trans.position;
+        cameraPos.x = player.trans.position.x;
+        cameraPos.y = player.trans.position.y + offsetY;
+        trans.position = cameraPos;
+
+        //this.transform.position = player.transform.position;
+    }
 }
--- RitmicSlime/Assets/Code/movePlayer.cs
+++ RitmicSlime/Assets/Code/movePlayer.cs
@@ -7,8 +7,10 @@ public class movePlayer : MonoBehaviour {
 	public Transform trans;
     public Rigidbody2D body;
     public float walkingSpeed;
-    public float jumpSpeed;
-
+    private float jumpForce = 200f;
+    public Animator animator;
+    private bool isFlip;
+    private bool escomida;
 	private void Awake(){
 		trans = this.trans;
 	}
@@ -16,7 +18,8 @@ public class movePlayer : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFlip = false;
+        escomida = false;
     }
 
     // Update is called once per frame
@@ -24,7 +27,7 @@ public class movePlayer : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         { // x-axis movement
-            body.velocity += jumpSpeed * Vector2.up;
+            body.AddForce(new Vector2(0f, jumpForce));
         }
 
         { // x-axis movement
@@ -33,13 +36,28 @@ public class movePlayer : MonoBehaviour {
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 speed += -walkingSpeed;
+                isFlip = false;
+                Vector3 theScale = trans.localScale;
+		        if(!isFlip){
+                    theScale.x = Mathf.Abs(theScale.x);
+	        	    trans.localScale = theScale;
+                }
+
             }
             if (Input.GetKey(KeyCode.RightArrow))
             {
+                Vector3 theScale = trans.localScale;
+                isFlip = true;
+		        if(isFlip){
+                    theScale.x = -1*Mathf.Abs(theScale.x);
+	        	    trans.localScale = theScale;
+                }
                 speed += walkingSpeed;
             }
             v.x = speed;
             body.velocity = v;
+            animator.SetFloat("Speed",speed);
+                    //animator.SetBool("Escomida",false);
         }
     }
 
@@ -48,11 +66,34 @@ public class movePlayer : MonoBehaviour {
 		var otherObject = collision.collider.gameObject;
 		if( otherObject.tag == "Magnifier" ) {
 			var scale = this.transform.localScale;
-			scale.y *= 2;
+			scale.y *= 1.15f;
 			this.transform.localScale = scale;
-			//otherObject.SetActive(false);
-			GameObject.Destroy(otherObject);
+			otherObject.SetActive(false);
+            var xPos = Random.Range(-3.5f, 5.6f);
+            var yPos = Random.Range(-1.5f, 5.2f);
+            var desiredPos = new Vector3(xPos, yPos, transform.position.z);
+            otherObject.transform.position = desiredPos;
+            var timer = 0.0f;
+            otherObject.SetActive(true);
+            this.jumpForce -= 10;
+		}
+
+        if( otherObject.tag == "Obstacle" ) {
+			var scale = this.transform.localScale;
+			scale.y *= 0.75f;
+			this.transform.localScale = scale;
+			otherObject.SetActive(false);
+            var xPos = Random.Range(-3.5f, 5.6f);
+            var yPos = Random.Range(-1.5f, 5.2f);
+            var desiredPos = new Vector3(xPos, yPos, transform.position.z);
+            var desiredPos2 = new Vector3(xPos+0.8f, yPos+0.8f, transform.position.z);
+            otherObject.transform.position = desiredPos;
+            var timer = 0.0f;
+            this.transform.position = desiredPos2;
+            this.jumpForce += 15;
+            otherObject.SetActive(true);
 		}
 
 	}
+
 }
